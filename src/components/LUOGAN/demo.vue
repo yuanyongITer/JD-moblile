f10692b67a866e1eee99ca74b32b389d71fee39c
Lg <1071843726@qq.com>
2017-11-22 10:25:33 +0800

    update

---
+++ src/components/LUOGAN/demo.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<mt-button type="primary" @click.native = "a">筛选</mt-button>
+		<mt-popup v-model="popupVisible" position="right" id="size">1
+		</mt-popup>
+	</div>
+</template>
+<script type="text/javascript">
+	export default{
+		data(){
+			return {
+				popupVisible:false,
+				selected:"",
+			}
+		},
+		mounted(){
+			let height = document.documentElement.clientHeight;
+			let width = document.documentElement.clientWidth - 80;
+			document.getElementById('size').style.width = width + "px";
+			document.getElementById('size').style.height = height + "px";
+		},
+		methods:{
+			a(){
+				this.popupVisible = true;
+			}
+		},
+	}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import demo from '@/components/LUOGAN/demo'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+    	path: '/demo',
+    	name: 'demo',
+    	component:demo
     }
   ]
 })
