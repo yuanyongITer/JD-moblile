82ecec1a192d9bfabd44ef8eee9dee3d50703ebb
MrWu <87551619@qq.com>
2017-11-26 14:54:05 +0800

    update

---
+++ src/components/WUSICONG/goodsdetail.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="all_class_flex">
+  	<div class="first_class_flex">
+  		<div class='firstDiv_class_oneTab' v-bind:class="{'active':oneActive}">商品介绍</div>
+  		<div class="firstDiv_class_midTab" v-bind:class="{'active':twoActive}">规格参数</div>
+  		<div class='firstDiv_class_secondTab'v-bind:style="{'active':threeActive}">包装售后</div>
+  	</div>
+  </div>
+</template>
+<script>
+	export default{
+		data() {
+			return{
+				oneActive:false,
+				twoActive:false,
+				threeActive:false,
+			}
+		},
+		mounted() {
+			this.oneActive = true;
+		},
+	}
+</script>
+<style scoped>
+	.all_class_flex
+	{
+		display: flex;
+		flex-direction: column;
+	}
+	.first_class_flex 
+	{
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-content:stretch;
+		flex-grow: 1;
+		padding: 0.5rem 0 0.5rem 0;
+	}
+	.firstDiv_class_oneTab{
+		margin-left: 2rem;
+	}
+	.firstDiv_class_secondTab{
+		margin-right: 2rem;
+	}
+	.firstDiv_class_midTab{
+	}
+	.active{
+		color: red;
+	}
+</style>
--- src/router/wusicong.js
+++ src/router/wusicong.js
@@ -1,8 +1,21 @@
 import PictureRotation from '@/components/WUSICONG/PictureRotation'
+import goodsdetail from '@/components/WUSICONG/goodsdetail'
+import goodsdetailtwo from '@/components/WUSICONG/goodsdetailtwo'
 const activity = [{
 	path: '/picturerotation',
     name: 'PictureRotation',
     component:PictureRotation
-}]
+},
+{
+	path: '/goodsdetail',
+    name: 'goodsdetail',
+    component:goodsdetail
+},
+{
+	path: '/goodsdetailtwo',
+    name: 'goodsdetailtwo',
+    component:goodsdetailtwo
+}
+]
 
 module.exports = activity;
