604d91b2c86a99d0867b225d150367589b74bdb6
cyq <2665646340@qq.com>
2017-11-27 21:05:33 +0800

    update

--- src/components/YANGXIAOYU/children/GoodsRating.vue
+++ src/components/YANGXIAOYU/children/GoodsRating.vue
@@ -5,46 +5,51 @@
 <template>
 	<div id="div_GoodsRating">
 		<div class="div_class_RatingLink">
-			<div class="div_class_Rowhead">
-				评价数(2334)
-			</div>
-			<div>好评度&nbsp;&nbsp;
-				<span class="all_class_Hightlight_Red">97%</span>
+			<p>评价数(2334)</p>
+			<div>
+				好评度&nbsp;&nbsp;
+				<span>97%</span>
 				<i class="iconfont icon-arrow-right"></i>
 			</div>
 		</div>
 		<div class="div_class_ratingContent" v-for="item in num" :key="item">
+			<div class="all_class_Border1px"></div>
 			<div class="div_class_Grid">
 				<div class="div_class_GridCol_Col6">
 					<img src="../../../../static/info_all_peisong.jpg" class="userImg">
 				</div>
 				<div class="div_class_GridCol_Colcenter">
 					<p class="p_class_userInfo">
-						<span>令*****龙 <mt-badge size="small" color="#888">PLUS</mt-badge></span>
-						<span class="all_class_Hightlight_Red">
-							<span  v-for="(star,index) in 5">★</span>
+						<span class="span_class_userName">令*****龙 <mt-badge size="small" color="#888">PLUS</mt-badge></span>
+						<span class="span_class_star">
+							<span v-for="(star,index) in 5">★</span>
 						</span>
 					</p>
 				</div>
 			</div>
 			<div class="div_class_Grid">
 				<div class="div_class_GridCol_Col100">
-					<p class="p_class_Intro">物流速度很快，一大早就收到了，试用了一下午，说说感觉吧，我是把我的5S换了，5用了3年了，手机很流畅就是电池待机不行，一天充好几回。关注8很久了，由于价格太高一直没下手，刚好前几天华为美十也上了，去看过，确实不错，有想入手的冲动，可是这两天关注8的时候看到京东优惠蛮大的，就果断入手了。</p>
+					<p class="p_class_Intro">
+						物流速度很快，一大早就收到了，试用了一下午，说说感觉吧，我是把我的5S换了，5用了3年了，手机很流畅就是电池待机不行，一天充好几回。关注8很久了，由于价格太高一直没下手，刚好前几天华为美十也上了，去看过，确实不错，有想入手的冲动，可是这两天关注8的时候看到京东优惠蛮大的，就果断入手了。
+					</p>
 					<div class="div_class_ActualPicture">
-						<img src="../../../../static/info_all_good.jpg" class="imgItem">
-						<img src="../../../../static/info_all_good.jpg" class="imgItem">
-						<img src="../../../../static/info_all_good.jpg" class="imgItem">
-						<img src="../../../../static/info_all_good.jpg" class="imgItem">
-					</div>
+		                <div class="div_class_GridCol_Col25" v-for="n in 4" :key="n">
+		                    <img src="../../../../static/info_all_good.jpg">
+		                </div>
+		            </div>
 				</div>
 			</div>
 			<p class="p_class_goodsSize">金色 , 64GB</p>
 		</div>
 		<div class="div_class_ratingBtn">
-			<div class="btnItem">查看全部评价
-			</div>
-			<div class="btnItem">购买咨询</div>
-		</div>
+			<div class="btnItem">
+		        <span>查看全部评价</span>
+		    </div>
+		    <div class="btnItem">
+		        <span>购买咨询</span>
+		    </div>
+	    </div>
+	    <div class="all_class_Border1px"></div>
 	</div>
 </template>
 <script>
@@ -63,75 +68,87 @@
 		}
 	}
 </script>
-<style scoped>
+<style lang="scss" scoped>
+	@import '../../../assets/css/Util.scss';
+  	@import '../../../assets/css/Common.scss';
 	#div_GoodsRating{
 		font-size: 0.8rem;
+		margin-bottom: 0.5rem;
+		padding-bottom: 1rem;
+		background: #ffffff;
+		.div_class_RatingLink{
+			@include setFjustify(space-between);
+			padding: 0rem 0.3rem 0.5rem;
+			p{
+				color: $gcolorGray;
+			}
+			i{
+				color: $gcolorGrayLight-1;
+			}
+			span{
+				color: $gcolorHighlightRed;
+			}
+		}
 	}
-	.div_class_RatingLink{
-		display: flex;
-		justify-content: space-between;
-		padding: 0rem 0.3rem 0.5rem;
-	}
-	.div_class_Center{
-		display: flex;
-		align-self: baseline;
-	}
-    /**顶部的样式**/
+	
+    /**底部的样式**/
     .div_class_ratingBtn{
-    	display: flex;
-		justify-content: space-between;
-		padding: 0.8rem 0 0.7rem;
-		border-bottom: 1px solid #F1F1F1;
+		@include setFjustify(space-between);
+		padding: 0.2rem 0rem 0.7rem;
+		/*border-bottom: 1px solid #F1F1F1;*/
+		.btnItem{
+		 	width: 45%;
+	    	text-align: center;
+	    	color: #A2A2A2;
+	    	border: 1px solid #F0F0F0;
+	    	border-radius:5px;
+	    	padding: 0.4rem 0;
+	    	font-size: 0.8rem;
+		}
     }
-    .btnItem{
-    	width: 45%;
-    	text-align: center;
-    	color: #A2A2A2;
-    	border: 1px solid #F0F0F0;
-    	border-radius:5px;
-    	padding: 0.4rem 0;
-    	font-size: 0.8rem;
-    }
+    
     /**评价详情样式**/
     .div_class_ratingContent{
-    	border-top: 1px solid #F1F1F1;
-    	padding: 5px 2px;
+    	padding: 0rem 0.2rem;
+    	.userImg{
+			width: 2rem;
+			height: 2rem;
+			border-radius:50%;
+			overflow:hidden;
+		}
+		.p_class_Intro{
+	        font-size: $gfontsizeSmall;
+	        overflow: hidden;
+	        text-overflow: -o-ellipsis-lastline;
+	        text-overflow: ellipsis;
+	        display: -webkit-box;
+	        -webkit-line-clamp: 2;
+	        -webkit-box-orient: vertical;
+	        margin-bottom: 0.5rem;
+	    }
+	    .p_class_userInfo{
+			@include setFjustify(space-between);
+			align-items: center;
+			padding: 0.4rem 0.2rem;
+			.span_class_userName{
+				font-size: $gfontsizeMini; 
+			}
+			.span_class_star{
+				color: $gcolorHighlightRed;
+			}
+		}
+		.div_class_ActualPicture{
+	        flex-wrap:wrap;
+	        display: flex;
+	        text-align: center;
+	        img{
+	          @include setWH(90%,95%);
+	        }
+	    }
+	    .p_class_goodsSize{
+			font-size: $gfontsizeSmall;
+			padding: 0rem 0.2rem 0.5rem;
+			color: $gcolorGrayLight-2;
+		}
     }
-	.userImg{
-		width: 2rem;
-		height: 2rem;
-		border-radius:50%;
-		overflow:hidden;
-	}
-	.p_class_Intro{
-        font-size: 0.6rem - 0.2rem ;
-        overflow: hidden;
-        text-overflow: -o-ellipsis-lastline;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-        margin-bottom: 0.5rem;
-    }
-	.p_class_userInfo{
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		padding: 0.4rem 0.2rem;
-	}
-	.div_class_ActualPicture{
-		display: flex;
-	    align-items: flex-start;
-	    flex-wrap: wrap;
-	    justify-content: space-between; 
-	}
-	.imgItem{
-		width: 23%;
-	}
-
-	.p_class_goodsSize{
-		margin: 0.4rem 0rem 0.2rem;
-		color: #c1c1c1;
-		font-size: 0.8rem;
-	}
 </style>
--- src/components/YANGXIAOYU/demo.vue
+++ src/components/YANGXIAOYU/demo.vue
@@ -1,98 +1,98 @@
 <template>
-  <div class='div_class_ShopDetail'>
-    <!--顶部-->
-    <mt-navbar class="div_class_NavBar" v-model="selected" >
-      <i class="iconfont icon-left-copy" @click="goBack()"></i>
-      <mt-tab-item id="div_id_Good">商品</mt-tab-item>
-      <mt-tab-item id="div_id_GoodDetail">详情</mt-tab-item>
-      <mt-tab-item id="div_id_GoodRating">评价</mt-tab-item>
-      <i class="iconfont icon-gengduo"></i>
-    </mt-navbar>
-    <!-- tabcontainer -->
-    <mt-tab-container v-model="selected" class="div_class_Container">
-      <mt-tab-container-item id="div_id_Good">
-        <div class="div_class_ShopContent">
-          <!--轮播图-->
-          <PictureRotation></PictureRotation>
-          <!--商品信息-->
-          <GoodsInfo></GoodsInfo>
-          <!--商品规格及配送-->
-          <GoodsDelivery></GoodsDelivery>
-          <!--商品评价-->
-          <GoodsRating></GoodsRating>
-          <!--店铺信息组件-->
-          <ShopInfo></ShopInfo>
-          <!--为你推荐商品或排行榜-->
-          <GoodsRank></GoodsRank>
-        </div>
-      </mt-tab-container-item>
-      <mt-tab-container-item id="div_id_GoodDetail">
-          <GoodsDetail></GoodsDetail>
-      </mt-tab-container-item>
-      <mt-tab-container-item id="div_id_GoodRating">
-        <RatingDetail></RatingDetail>
-      </mt-tab-container-item>
-    </mt-tab-container>
-  </div>
+    <div class='div_class_ShopDetail'>
+        <!--顶部-->
+        <mt-navbar class="div_class_NavBar" v-model="selected" >
+            <i class="iconfont icon-left-copy" @click="goBack()"></i>
+            <mt-tab-item id="div_id_Good">商品</mt-tab-item>
+            <mt-tab-item id="div_id_GoodDetail">详情</mt-tab-item>
+            <mt-tab-item id="div_id_GoodRating">评价</mt-tab-item>
+            <i class="iconfont icon-gengduo"></i>
+        </mt-navbar>
+        <!-- tabcontainer -->
+        <mt-tab-container v-model="selected" class="div_class_Container">
+            <mt-tab-container-item id="div_id_Good">
+                <div class="div_class_ShopContent">
+                    <!--轮播图-->
+                    <PictureRotation></PictureRotation>
+                    <!--商品信息-->
+                    <GoodsInfo></GoodsInfo>
+                    <!--商品规格及配送-->
+                    <GoodsDelivery></GoodsDelivery>
+                    <!--商品评价-->
+                    <GoodsRating></GoodsRating>
+                    <!--店铺信息组件-->
+                    <ShopInfo></ShopInfo>
+                    <!--为你推荐商品或排行榜-->
+                    <GoodsRank></GoodsRank>
+                </div>
+            </mt-tab-container-item>
+            <mt-tab-container-item id="div_id_GoodDetail">
+                <GoodsDetail></GoodsDetail>
+            </mt-tab-container-item>
+            <mt-tab-container-item id="div_id_GoodRating">
+                <RatingDetail></RatingDetail>
+            </mt-tab-container-item>
+        </mt-tab-container>
+    </div>
 </template>
 <script type="text/javascript">
-  import GoodsInfo from './children/GoodsInfo'
-  import GoodsDelivery from './children/GoodsDelivery'
-  import GoodsRating from './children/GoodsRating'
-  import ShopInfo from './children/ShopInfo'
-  import GoodsRank from './children/GoodsRank'
-  import PictureRotation from '../WUSICONG/PictureRotation'
-  import GoodsDetail from '../WUSICONG/GoodsDetail'
-  import RatingDetail from './RatingDetail'
-  export default {
-    components: {
-      GoodsInfo,
-      GoodsDelivery,
-      GoodsRating,
-      ShopInfo,
-      GoodsRank,
-      PictureRotation,
-      RatingDetail,
-      GoodsDetail
-    },
-    data() {
-      return {
-        selected: 'div_id_Good',
-      }
-    },
-    mounted() {
+    import GoodsInfo from './children/GoodsInfo'
+    import GoodsDelivery from './children/GoodsDelivery'
+    import GoodsRating from './children/GoodsRating'
+    import ShopInfo from './children/ShopInfo'
+    import GoodsRank from './children/GoodsRank'
+    import PictureRotation from '../WUSICONG/PictureRotation'
+    import GoodsDetail from '../WUSICONG/GoodsDetail'
+    import RatingDetail from './RatingDetail'
+    export default {
+        components: {
+            GoodsInfo,
+            GoodsDelivery,
+            GoodsRating,
+            ShopInfo,
+            GoodsRank,
+            PictureRotation,
+            RatingDetail,
+            GoodsDetail
+        },
+        data() {
+            return {
+                selected: 'div_id_Good',
+            }
+        },
+        mounted() {
 
-    },
-    methods: {
-      goBack(){
-        this.$router.push('/demo');
-      }
-    },
-  }
+        },
+        methods: {
+            goBack(){
+                this.$router.push('/demo');
+            }
+        },
+    }
 
 </script>
 <style lang='scss'>
-  .div_class_NavBar{
-    display: flex;
-    align-items: center;
-    z-index: 101 ;
-    position: fixed;
-    top: 0;
-    width: 100%;
-
-    i{
-      width: 2.5rem;
+    @import '../../assets/css/Common.scss';
+    .div_class_ShopDetail {
+        width: 100%;
+        .div_class_ShopContent {
+            width: 96%;
+            margin: auto;
+        }
     }
-  }
-  .div_class_Container{
-    margin-top: 46px;
-  }
-  .div_class_ShopDetail {
-    width: 100%;
-    .div_class_ShopContent {
-      width: 96%;
-      margin: auto;
+    .div_class_NavBar{
+        display: flex;
+        align-items: center;
+        z-index: 101 ;
+        position: fixed;
+        top: 0;
+        width: 100%;
+        i{
+          width: 2.5rem;
+        }
     }
-  }
-
+    .div_class_Container{
+        margin-top: 46px;
+        /*background: $gcolorBgGray;*/
+    }
 </style>
